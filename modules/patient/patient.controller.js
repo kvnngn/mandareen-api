4570b20e95daf91bf595d60f2a07ed8e4a681163
Ben Ammar Sofiane <sofiane.benammar@epitech.eu>
2018-05-13 18:12:53 +0200

    Route patient - Login

--- modules/patient/patient.controller.js
+++ modules/patient/patient.controller.js
@@ -6,32 +6,34 @@ var models = require("../../models/index");
 module.exports = {
     patientLogin: function(req, res) {
         console.log(req.body);
-        console.log(req);
+        //console.log(req);
         var email = req.body.email;
         var password = req.body.password;
-
         if(email == null || password == null) {
             return res.status(400).json({'error': 'missing parameters'});
         }
-
         models.Patient.findOne({
             attributes: ['id', 'pass'],
             where: {email: email}
         })
         .then(function(patientFound) {
             if(patientFound) {
-                bcrypt.compare(password, patientFound.pass, function(errBycrypt, resBycrypt) {
-                    if(resBycrypt) {
+                /* ///// A décomenter lorsque les mots de passe seront hashés en bdd \\\\\ */
+                //bcrypt.compare(password, patientFound.pass, function(errBycrypt, resBycrypt) {
+                    //console.log(resBycrypt);
+                    if(password == patientFound.pass) {
+
                         return res.status(200).json({
-                            'token': jwtUtils.generateTokenForPatient(patientFound)
+                            'message': 'Utilisateur connecté'
+                            //'token': jwtUtils.generateTokenForPatient(patientFound)
                         });
                     }
                     else {
                         return res.status(403).json({"error": "invalid password"});
                     }
-                });
+                //});
             }
-            return res.status(404).json({'error': 'patient not exist in DB'});
+            return res.status(404).json({'error': 'patient does not exist in DB'});
         })
         .catch(function(err) {
             return res.status(500).json({'error': 'unable to verify patient'});
@@ -52,5 +54,14 @@ module.exports = {
         })
         .then(function(patient) { return res.json(patient); })
         .catch(next);
+    },
+    addAdmin: function(req, res) {
+        var Newlogin = req.body.login;
+        var password = req.body.password;
+        var firstname = req.body.firstname;
+        var lastname = req.body.lastname;
+        if(Newlogin == null || password == null || firstname == null || lastname == null) {
+            return res.status(400).json({'error': 'missing paramaters'});
+        }
     }
 };
