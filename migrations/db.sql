c50ddc0db3daa5b497babc0f0931c8e238c1d63e
Vincent <vincent.iturri@epitech.eu>
2018-07-02 18:18:01 +0000

    Update db.sql (pro update contract ended test)

--- migrations/db.sql
+++ migrations/db.sql
@@ -111,12 +111,12 @@ INSERT INTO `pro` (`email`, `pass`, `civ`, `firstname`, `lastname`, `city`,`zipc
 VALUES  ('alex.terrieur@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Alex', 'Terrieur', 'Paris', '50', '35', '0611821800', 'Doctor', 1, CURRENT_DATE, CURRENT_DATE + INTERVAL 1 MONTH),
         ('alain.terrieur@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Alain', 'Terrieur', 'Paris', '50', '35', '0611821801', 'Doctor', 3, CURRENT_DATE , CURRENT_DATE + INTERVAL 1 MONTH),
         ('amanda.rine@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'Mme', 'Amanda', 'Rine', 'Le Kremlin-Bicêtre', '93', '34', '0611223344', 'Vendeur de BK', 2, CURRENT_DATE , CURRENT_DATE + INTERVAL 1 MONTH),
-        ('victor.hugo@test.fr', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Victor', 'Hugo', 'La Mouche', '50', '1234', '0605030320', 'Vendeur de BK', 1, CURRENT_DATE - INTERVAL 10 MONTH, CURRENT_DATE + INTERVAL 10 YEAR);
+        ('victor.hugo@test.fr', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Victor', 'Hugo', 'La Mouche', '50', '1234', '0605030320', 'Vendeur de BK', 1, CURRENT_DATE - INTERVAL 10 MONTH, CURRENT_DATE - INTERVAL 8 MONTH);
 
 INSERT INTO  `patient` (`email`, `pass`, `civ`, `firstname`, `lastname`, `birthdate`)
 VALUES  ('jean.ko@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Jean', 'Ko', CURRENT_DATE - INTERVAL 20 YEAR),
         ('manda.reen@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'Mme', 'Manda', 'Rine', CURRENT_DATE - INTERVAL 18 YEAR),
-        ('test.mandareen@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Test', 'Mandareen', CURRENT_DATE),
+        ('test.mandareen@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'M', 'Test', 'Mandareen', CURRENT_DATE + 1 YEAR),
         ('test.refused@gmail.com', '$2a$05$WkcMIl.lXO8Qnz./o2l4HeLssZ.OYcHDZMVzXqXBtLhQshofMRfmy', 'Mme', 'Test', 'Refused', CURRENT_DATE );
 
 INSERT INTO `cares` (`sickness_name`)
