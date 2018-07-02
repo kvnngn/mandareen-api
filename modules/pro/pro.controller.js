00d8206eb7daecaa3960e43f3b48d7c8847070d7
Nguyen Kévin <kevin3.nguyen@epitech.eu>
2018-07-02 14:28:08 +0200

    update pro OK

--- modules/pro/pro.controller.js
+++ modules/pro/pro.controller.js
@@ -153,4 +153,64 @@ module.exports = {
             return res.status(500).json({'error': 'unable to verify patient'});
         });
     },
+    update: function(req, res, next) {
+        console.log("update");
+
+        const pro = req.body.pro;
+        const updatePro = req.body;
+
+        return models.Pro.update(
+            {
+                firstname: updatePro.firstname,
+                lastname: updatePro.lastname,
+                phone: updatePro.phone
+            },
+            {where: {id: pro.id}}
+        )
+        .then(function() { return res.json({res: "OK"}); })
+        .catch(function(err) {
+            console.log('Error verify pro:');
+            console.log('Log : ' + err);
+            return res.status(500).json({'error': 'unable to verify pro'});
+        });
+    },
+    updateEmail: function(req, res, next) {
+        console.log("updateEmail");
+
+        const email = req.body.email;
+
+        return models.Pro.update(
+            {email: email},
+            {where: {id: req.body.pro_id}}
+        )
+        .then(function() { return res.json({res: "OK"}); })
+        .catch(function(err) {
+            console.log('Error verify pro:');
+            console.log('Log : ' + err);
+            return res.status(500).json({'error': 'unable to verify pro'});
+        });
+    },
+    updatePassword: function(req, res, next) {
+        console.log("updatePassword");
+
+        bcrypt.hash(req.body.password, 5, function(err, bcryptedPassword) {
+            return models.Pro.find({where: {id: req.body.pro_id}})
+            .then(function(proFound) {
+                bcrypt.compare(req.body.password, proFound.pass, function(errBycrypt, resBycrypt) {
+                    if(resBycrypt) {
+                        bcrypt.hash(req.body.new_password, 5, function(err, bcryptedPassword) {
+                            console.log(bcryptedPassword);
+                            return models.Pro.update(
+                                {pass: bcryptedPassword},
+                                {where: {id: req.body.pro_id}}
+                            )
+                            .then(function() { return res.json("OK"); })
+                        });
+                    }
+                    else {return res.status(403).json({"error": "invalid password"});}
+                });
+            })
+            .catch(next);
+        });
+    }
 };
--- modules/pro/pro.routes.js
+++ modules/pro/pro.routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const proController = require("./pro.controller");
 
 router.route("/pro/auth/login/").post(proController.login);
+    router.route("/pro/account").put(proController.update);
+    router.route("/pro/account/email").put(proController.updateEmail);
+    router.route("/pro/account/password").put(proController.updatePassword);
 router.route("/pro/auth/register/").post(proController.register);
 router.route("/pro/patient/create/").post(proController.addPatient);
 
