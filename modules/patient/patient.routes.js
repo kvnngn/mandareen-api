e92d4ef4f634010ba0eff3a6cd941dbcc0401539
Kévin Nguyen <kevin3.nguyen@epitech.eu>
2018-10-06 15:44:33 +0900

    fix route on patient, add createDiary

--- modules/patient/patient.routes.js
+++ modules/patient/patient.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const patientController = require("./patient.controller");
 
 router.route("/patient/login/").post(patientController.login);
-router.route("/patient/diary/").post(patientController.create);
+router.route("/patient/diary/").post(patientController.createDiary);
 router.route("/patient/diary/").put(patientController.updateDiary);
 router.route("/patients/account/patients/:id").get(patientController.findById);
 router.route("/patient/diaries/:id").get(patientController.getAllPatientDiaries);
