025036c245ac8aca8a2e603258729e0fabc5e297
Kévin Nguyen <kevin3.nguyen@epitech.eu>
2018-10-08 00:39:29 +0900

    create reports, get report by patient

--- modules/pro/reports/reports.controller.js
+++ modules/pro/reports/reports.controller.js
@@ -8,8 +8,53 @@ module.exports = {
     getReportsByPro: function(req, res, next) {
         debug("getReportsByPro");
 
-        return models.Report_pro.findAll({where: {pro_id: req.params.id}})
+        console.log(req.params.id);
+        return models.Report_pro.findAll(
+            {
+                where: {pro_id: req.params.id},
+                attributes: ['creation_date', 'content'],
+                include: [{
+                    model: models.Patient,
+                    attributes: {exclude: ['password']}
+                }]
+            }
+        )
         .then(function(reports) { return res.json(reports); })
         .catch(next);
+    }, getReportsByPatient: function(req, res, next) {
+        debug("getReportsByPatient");
+
+        console.log(req.params.id);
+        return models.Report_pro.findAll(
+            {
+                where: {patient_id: req.params.id},
+                attributes: ['creation_date', 'content'],
+                include: [{
+                    model: models.Patient,
+                    attributes: {exclude: ['password']}
+                }]
+            }
+        )
+        .then(function(reports) { return res.json(reports); })
+        .catch(next);
+    },
+    create: function(req, res, next) {
+        debug("create");
+
+        return Promise.resolve()
+        .then(createReport)
+        .then((followup) => {res.json(followup)})
+        .catch(next);
+
+
+        function createReport() {
+            return models.Report_pro.create({
+                date: req.body.date,
+                patient_id: req.body.patient_id,
+                pro_id: req.body.pro_id,
+                content: req.body.content
+            })
+        }
     }
+
 };
