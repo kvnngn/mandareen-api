e9ca7baab8946e23566d85a058925058cf87f3fe
NGUYEN Kévin <kevin3.nguyen@epitech.eu>
2019-01-13 13:58:39 +0100

    delete console.log

--- modules/patient/patient.controller.js
+++ modules/patient/patient.controller.js
@@ -4,15 +4,16 @@ var models = require("../../models/index");
 const debug = require("debug")("app:patient.controller");
 
 function splitTime(time) {
-        var returnTime = new Date();
-        var split = time.split(':');
+    var returnTime = new Date();
+    var split = time.split(':');
 
-        returnTime.setHours(+split[0]);
-        returnTime.setMinutes(split[1]);
-        returnTime.setSeconds(split[2]);
+    returnTime.setHours(+split[0]);
+    returnTime.setMinutes(split[1]);
+    returnTime.setSeconds(split[2]);
 
-        return returnTime;
-    }
+    return returnTime;
+}
+
 function timeToString(time) {
     return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
 }
@@ -37,7 +38,7 @@ module.exports = {
     },
 
     updateDiary: function (req, res) {
-        console.log("Update Diary");
+        debug("Update Diary");
         return models.Diary.update({
                 content: req.body.content
             },
@@ -45,7 +46,6 @@ module.exports = {
                 where: {id: req.body.id}
             })
             .then(function (result) {
-                console.log(result);
                 return res.json(result);
             })
             .catch(function (err) {
@@ -77,13 +77,11 @@ module.exports = {
                                 'patient': patientFound,
                                 'token': jwtUtils.generateTokenForPro(patientFound)
                             });
-                        }
-                        else {
+                        } else {
                             return res.status(403).json({"error": "invalid password"});
                         }
                     });
-                }
-                else {
+                } else {
                     return res.status(404).json({'error': 'User not found'});
                 }
             })
@@ -109,21 +107,20 @@ module.exports = {
     },
 
     getAllRecipesNames: function (req, res, next) {
-        console.log("getAllRecipesNames");
+        debug("getAllRecipesNames");
         return models.Recipes.findAll({
             attributes: ['id', 'name', 'image'],
             offset: parseInt(req.params.offset),
             limit: 3
         })
             .then(function (recipes) {
-                console.log(recipes);
                 return res.json(recipes);
             })
             .catch(next);
     },
 
     getRecipeDetails: function (req, res, next) {
-        console.log("getRecipeDetails");
+        debug("getRecipeDetails");
         return models.Recipes.find({
             attributes: ['id', 'name', 'description', 'image', 'nb_cal', 'ingredients'],
             where: {
@@ -131,7 +128,6 @@ module.exports = {
             }
         })
             .then(function (recipes) {
-                console.log(recipes);
                 return res.json(recipes);
             })
             .catch(next);
@@ -164,11 +160,13 @@ module.exports = {
                 id: req.params.id
             }
         })
-        .then(function(patient) { return res.json(patient); })
-        .catch(next);
+            .then(function (patient) {
+                return res.json(patient);
+            })
+            .catch(next);
     },
 
-    timePassedOnPage: function(req, res, next){
+    timePassedOnPage: function (req, res, next) {
         debug("timePassedOnPage");
 
         var date = req.body.date;
@@ -179,126 +177,130 @@ module.exports = {
         return models.Stats.findOrCreate({
             where: {
                 id_patient: patient_id,
-                report_date: date 
+                report_date: date
             },
             defaults: {
                 id_patient: patient_id,
                 report_date: date
             }
         })
-        .then(function(result) {
-            console.log(result);
-            if (page == "Diary") {
-                models.Stats.find({
-                    attributes: ['diary_time'],
-                    where: {
-                        id_patient: patient_id,
-                        report_date: date
-                    }
-                })
-                .then(function(diary_time) {
-                    if (!diary_time.dataValues.diary_time)
-                        return models.Stats.update({
-                            diary_time: time },
-                            { where: {
-                                id_patient: patient_id,
-                                report_date: date
-                            }
-                        })
-                    else {
-                        var dbTime = splitTime(diary_time.dataValues.diary_time);
-                        var applicationTime = splitTime(time);
-                        
-                        var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(), 
-                            dbTime.getMinutes() + applicationTime.getMinutes(), 
-                            dbTime.getSeconds() + applicationTime.getSeconds());
-                        return models.Stats.update({
-                            diary_time: timeToString(totalTime)
-                            },
-                            { where: {
-                                id_patient:patient_id,
-                                report_date: date
-                            }
-                        })
-                    }
-                })
-            }
+            .then(function (result) {
+                if (page == "Diary") {
+                    models.Stats.find({
+                        attributes: ['diary_time'],
+                        where: {
+                            id_patient: patient_id,
+                            report_date: date
+                        }
+                    })
+                        .then(function (diary_time) {
+                            if (!diary_time.dataValues.diary_time)
+                                return models.Stats.update({
+                                        diary_time: time
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
+                            else {
+                                var dbTime = splitTime(diary_time.dataValues.diary_time);
+                                var applicationTime = splitTime(time);
 
-            else if (page == "Recipes") {
-                models.Stats.find({
-                    attributes: ['recipe_time'],
-                    where: {
-                        id_patient: patient_id,
-                        report_date: date
-                    }
-                })
-                .then(function(recipe_time) {
-                    if (!recipe_time.dataValues.recipe_time)
-                        return models.Stats.update({
-                            recipe_time: time },
-                            { where: {
-                                id_patient: patient_id,
-                                report_date: date
+                                var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(),
+                                    dbTime.getMinutes() + applicationTime.getMinutes(),
+                                    dbTime.getSeconds() + applicationTime.getSeconds());
+                                return models.Stats.update({
+                                        diary_time: timeToString(totalTime)
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
                             }
                         })
-                    else {
-                        var dbTime = splitTime(recipe_time.dataValues.recipe_time);
-                        var applicationTime = splitTime(time);
-                        
-                        var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(), 
-                            dbTime.getMinutes() + applicationTime.getMinutes(), 
-                            dbTime.getSeconds() + applicationTime.getSeconds());
-                        return models.Stats.update({
-                            recipe_time: timeToString(totalTime)
-                            },
-                            { where: {
-                                id_patient:patient_id,
-                                report_date: date
-                            }
-                        })
-                    }
-                })
-            }
+                } else if (page == "Recipes") {
+                    models.Stats.find({
+                        attributes: ['recipe_time'],
+                        where: {
+                            id_patient: patient_id,
+                            report_date: date
+                        }
+                    })
+                        .then(function (recipe_time) {
+                            if (!recipe_time.dataValues.recipe_time)
+                                return models.Stats.update({
+                                        recipe_time: time
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
+                            else {
+                                var dbTime = splitTime(recipe_time.dataValues.recipe_time);
+                                var applicationTime = splitTime(time);
 
-            else if (page == "Total"){
-                models.Stats.find({
-                    attributes: ['app_time'],
-                    where: {
-                        id_patient: patient_id,
-                        report_date: date
-                    }
-                })
-                .then(function(app_time) {
-                    if (!app_time.dataValues.app_time)
-                        return models.Stats.update({
-                            app_time: time },
-                            { where: {
-                                id_patient: patient_id,
-                                report_date: date
+                                var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(),
+                                    dbTime.getMinutes() + applicationTime.getMinutes(),
+                                    dbTime.getSeconds() + applicationTime.getSeconds());
+                                return models.Stats.update({
+                                        recipe_time: timeToString(totalTime)
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
                             }
                         })
-                    else {
-                        var dbTime = splitTime(app_time.dataValues.app_time);
-                        var applicationTime = splitTime(time);
-                        
-                        var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(), 
-                            dbTime.getMinutes() + applicationTime.getMinutes(), 
-                            dbTime.getSeconds() + applicationTime.getSeconds());
-                        return models.Stats.update({
-                            app_time: timeToString(totalTime)
-                            },
-                            { where: {
-                                id_patient:patient_id,
-                                report_date: date
+                } else if (page == "Total") {
+                    models.Stats.find({
+                        attributes: ['app_time'],
+                        where: {
+                            id_patient: patient_id,
+                            report_date: date
+                        }
+                    })
+                        .then(function (app_time) {
+                            if (!app_time.dataValues.app_time)
+                                return models.Stats.update({
+                                        app_time: time
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
+                            else {
+                                var dbTime = splitTime(app_time.dataValues.app_time);
+                                var applicationTime = splitTime(time);
+
+                                var totalTime = new Date(null, null, null, dbTime.getHours() + applicationTime.getHours(),
+                                    dbTime.getMinutes() + applicationTime.getMinutes(),
+                                    dbTime.getSeconds() + applicationTime.getSeconds());
+                                return models.Stats.update({
+                                        app_time: timeToString(totalTime)
+                                    },
+                                    {
+                                        where: {
+                                            id_patient: patient_id,
+                                            report_date: date
+                                        }
+                                    })
                             }
                         })
-                    }
-                })
-            }
-            return res.status(200).json({
-                '200': 'Ok'
-            });
-        })
-        .catch(next);
+                }
+                return res.status(200).json({
+                    '200': 'Ok'
+                });
+            })
+            .catch(next);
     }
 };
